fb5189450d136098f3e5932a298e5c3d96a337ca
NathaliaCondelloCarvalho <eduardo.nathalia2002@gmail.com>
2022-03-15 22:47:29 -0300

    Registro de Atividades: curso JS (ONE-Oracle/Alura)

---
+++ meu-primeiro-programa.js
@@ -0,0 +1,9 @@
+console.log("Meu primeiro programa: trabalhando com variáveis");
+
+const idade = 26;
+
+console.log("a idade é "+idade);
+console.log(idade+2);
+
+const idadeSomada = idade+2;
+console.log("a idade mais 2 é " + idadeSomada);
